001440b38e1a741107cc82193be0cbc1547919b7
james chalmers <james.chalmers184@gmail.com>
2021-11-24 12:57:06 +0000

    fluentvalidation added and validators added

---
+++ CoffeeClub/Validators/AddCoffeeValidator.cs
@@ -0,0 +1,21 @@
+﻿using Entities.DTOs;
+using FluentValidation;
+
+namespace CoffeeClub.Validators
+{
+    public class AddCoffeeValidator : AbstractValidator<CoffeeForCreationDTO>
+    {
+        public AddCoffeeValidator()
+        {
+            RuleFor(x => x.CoffeeName).NotNull().NotEmpty().WithMessage("CoffeeName is required.");
+            RuleFor(x => x.CoffeeName).MinimumLength(8);
+            RuleFor(x => x.CoffeeName).MaximumLength(20);
+            RuleFor(x => x.CoffeePrice).NotNull().NotEmpty().WithMessage("CoffeePrice is required.");
+            RuleFor(x => x.CoffeePrice).GreaterThan(0);
+            RuleFor(x => x.CoffeePrice).LessThan(100.00);
+            RuleFor(x => x.CountryOfOrigin).NotNull().NotEmpty().WithMessage("CountryOfOrigin is required.");
+            RuleFor(x => x.CountryOfOrigin).MinimumLength(3);
+            RuleFor(x => x.CountryOfOrigin).MaximumLength(20);
+        }
+    }
+}
---
+++ CoffeeClub/Validators/UpdateCommentValidator.cs
@@ -0,0 +1,19 @@
+﻿using Entities.DTOs;
+using FluentValidation;
+
+namespace CoffeeClub.Validators
+{
+    public class UpdateCommentValidator : AbstractValidator<CommentsForCreationDTO>
+    {
+        public UpdateCommentValidator()
+        {
+            RuleFor(x => x.Comment).NotNull().NotEmpty().WithMessage("Comment is required.");
+            RuleFor(x => x.Comment).MinimumLength(8);
+            RuleFor(x => x.Comment).MaximumLength(100);
+            RuleFor(x => x.Rating).NotNull().NotEmpty().WithMessage("Rating is required.");
+            RuleFor(x => x.Rating).GreaterThan(0);
+            RuleFor(x => x.Rating).LessThan(6);
+            RuleFor(x => x.DateCreated).NotNull().NotEmpty().WithMessage("DateCreated is required.");
+        }
+    }
+}
